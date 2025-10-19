02ef18678004908c94f84b480b8b29d462c06dec
RybchynskyiDaniil <rybchynskyidaniil@gmail.com>
2025-10-19 15:20:34 +0000

    Deploying to gh-pages from @ RybchynskyiDaniil/goit-js-hw-11@5cab3dff1bbc7320bdf82941d2f0571e754b1c8f 🚀

--- index.js
+++ index.js
@@ -1,2 +1,24 @@
-/* empty css                      */(function(){const t=document.createElement("link").relList;if(t&&t.supports&&t.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))i(e);new MutationObserver(e=>{for(const r of e)if(r.type==="childList")for(const o of r.addedNodes)o.tagName==="LINK"&&o.rel==="modulepreload"&&i(o)}).observe(document,{childList:!0,subtree:!0});function s(e){const r={};return e.integrity&&(r.integrity=e.integrity),e.referrerPolicy&&(r.referrerPolicy=e.referrerPolicy),e.crossOrigin==="use-credentials"?r.credentials="include":e.crossOrigin==="anonymous"?r.credentials="omit":r.credentials="same-origin",r}function i(e){if(e.ep)return;e.ep=!0;const r=s(e);fetch(e.href,r)}})();
+import{a as c,i as d}from"./assets/vendor-CHMhaTs2.js";(function(){const e=document.createElement("link").relList;if(e&&e.supports&&e.supports("modulepreload"))return;for(const t of document.querySelectorAll('link[rel="modulepreload"]'))n(t);new MutationObserver(t=>{for(const s of t)if(s.type==="childList")for(const r of s.addedNodes)r.tagName==="LINK"&&r.rel==="modulepreload"&&n(r)}).observe(document,{childList:!0,subtree:!0});function i(t){const s={};return t.integrity&&(s.integrity=t.integrity),t.referrerPolicy&&(s.referrerPolicy=t.referrerPolicy),t.crossOrigin==="use-credentials"?s.credentials="include":t.crossOrigin==="anonymous"?s.credentials="omit":s.credentials="same-origin",s}function n(t){if(t.ep)return;t.ep=!0;const s=i(t);fetch(t.href,s)}})();const u="52837219-31e3d366b905b38697491d842";c.defaults.baseURL="https://pixabay.com";function f(a){return c.get("/api/",{params:{key:u,q:a,image_type:"photo",orientation:"horizontal",safesearch:!0}}).then(e=>e.data.hits).catch(e=>{console.log(e.message)})}const l={formEl:document.querySelector(".form"),galleryEl:document.querySelector(".gallery"),loaderEl:document.querySelector(".loader")};let o="";function g(a){for(const e of a)o+=`<li class="gallery-item">
+            <a class="gallery-link" href=${e.largeImageURL}>
+                <img class="gallery-image" src=${e.webformatURL} alt=${e.tags}>
+            </a>
+            <ul class="image-stats-list">
+                <li class="image-stats-item">
+                    <h3 class="image-stats-title">Likes</h3>
+                    <p class="image-stats">${e.likes}</p>
+                </li>
+                <li class="image-stats-item">
+                    <h3 class="image-stats-title">Views</h3>
+                    <p class="image-stats">${e.views}</p>
+                </li>
+                <li class="image-stats-item">
+                    <h3 class="image-stats-title">Comments</h3>
+                    <p class="image-stats">${e.comments}</p>
+                </li>
+                <li class="image-stats-item">
+                    <h3 class="image-stats-title">Downloads</h3>
+                    <p class="image-stats">${e.downloads}</p>
+                </li>
+            </ul>
+        </li>`;l.galleryEl.insertAdjacentHTML("beforeend",o)}function p(){o="",l.galleryEl.innerHTML=""}function h(){l.loaderEl.classList.contains("visually-hidden")&&l.loaderEl.classList.remove("visually-hidden")}function m(){l.loaderEl.classList.contains("visually-hidden")||l.loaderEl.classList.add("visually-hidden")}function y(a){a.preventDefault();const e=a.target.elements[0].value;e===""&&d.error({message:"Please write something",position:"topRight"}),p(),h(),f(e).then(i=>{g(i)}).catch(i=>{console.log(i)}).finally(()=>{m()})}m();l.formEl.addEventListener("submit",y);
 //# sourceMappingURL=index.js.map
